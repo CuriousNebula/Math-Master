7775b1f97d23ff5dbd514b42cf3d9a6d94858d0c
AshCodes77 <ashfreak777@gmail.com>
2025-01-19 02:57:27 +0530

    Remove unnecessary NativeScript files and folders

--- app/views/hero-page.ts
+++
@@ -1,7 +0,0 @@
-import { NavigatedData, Page } from '@nativescript/core';
-import { HeroViewModel } from '../viewmodels/hero-view-model';
-
-export function onNavigatingTo(args: NavigatedData) {
-    const page = <Page>args.object;
-    page.bindingContext = new HeroViewModel();
-}
